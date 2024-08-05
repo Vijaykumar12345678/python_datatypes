8408df7a20c269b794533f60f2692820bffe3208
vijay1 <vijaykumarynh7@gmail.com>
2024-08-05 14:34:09 +0530

    [Vijay]:Python program to create different datatypes  a tuple.

---
+++ tuples/create_differentdatatypes.py
@@ -0,0 +1,27 @@
+'''
+@Author: Vijay Kumar M N
+@Date: 2024-08-05
+@Last Modified by: Vijay Kumar M N
+@Last Modified: 2024-08-05
+@Title: Python program to create a tuple.
+'''
+
+def create_tuples():
+    """
+    Description:
+    This function creates a tuple and prints its elements.
+    """
+    
+   
+    sample_tuple = ('apple', 'banana', 'cherry', 42, 3.14, True)
+    
+   
+    print("Created Tuple:", sample_tuple)
+    
+ 
+
+def main():
+    create_tuples()
+
+if __name__ == "__main__":
+    main()
