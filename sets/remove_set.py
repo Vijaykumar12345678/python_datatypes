9209c9956992f4ff0cc49c91bbc8fe465acf223d
vijay1 <vijaykumarynh7@gmail.com>
2024-08-05 14:54:01 +0530

    [Vijay]:Python program to create and remove element  a set.

---
+++ sets/remove_set.py
@@ -0,0 +1,15 @@
+'''
+@Author: Vijay Kumar M N
+@Date: 2024-08-05
+@Last Modified by: Vijay Kumar M N
+@Last Modified: 2024-08-05
+@Title: Python program to create and remove element  a set.
+'''
+def main():
+    # Sample list of elements
+    elements = {1, 2, 3, 4, 4, 5, 6, 7, 1, 2, 5}
+    number=int(input("Enter the number to remove : "))
+    elements.remove(number)
+    print(elements)
+if __name__ == "__main__":
+    main()
